d1fafe1091f57824066df57ca5d97bd2e41b4c91
pmutunga <paskwa.mutunga@ipsafari.com>
2019-01-11 16:05:17 -0500

    event-id

--- assets/javascript/event.js
+++ assets/javascript/event.js
@@ -20,7 +20,10 @@ var eventState = "";
 var stateCode = "";
 var eventCity="";
 var searchURL="";
-var keyWord = "";
+var keyword = "";
+var eventID = "";
+
+
 var ticketmasterCountries = [
     {"code":"US","name":"united states of america"},		
     {"code":"AD","name":"andorra"},		
@@ -171,10 +174,10 @@ function keywordSearch(){
      
     //capture search values
  
-     keywordSearch = $("#key-word").val().trim().toLowerCase();
-     console.log("Key word is: " + keywordSearch);
+     keyword = $("#key-word").val().trim().toLowerCase();
+     console.log("Key word is: " + keyword);
 
-     searchURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp&keyWord=" + keywordSearch;
+     searchURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp&keyWord=" + keyword;
 
      console.log(searchURL);
     };
@@ -194,12 +197,18 @@ function getEvents(){
         // store the data from the AJAX request in the results variable
         eventResults = json._embedded.events;
 
+        //Update event Info
+        
+        var eventSummary = $("#event-info");
+        eventSummary.addClass("text-center");
+        eventSummary.text("Explore " + eventResults.length + " events.");
         
         for(var i=0; i<eventResults.length; i++){
 
             if(Array.isArray(eventResults[i].priceRanges)){
                 
                 console.log("Explore " + eventResults.length + " events");
+                console.log("EventID is " + eventResults[i].id);
                 console.log("Event number is: " +i);
                 console.log("Event name is :" + eventResults[i].name);//1
                 console.log("Event image :" + eventResults[i].images[3].url);//2
@@ -214,7 +223,7 @@ function getEvents(){
 
                 //assign event variables based on data
                 eventName = eventResults[i].name;
-                eventImage = eventResults[i].images[0].url;
+                eventImage = eventResults[i].images[5].url;
                 eventCurrency = eventResults[i].priceRanges[0].currency;
                 EventMinPrice = eventResults[i].priceRanges[0].min;
                 eventMaxPrice = eventResults[i].priceRanges[0].max;
@@ -225,13 +234,12 @@ function getEvents(){
                 
                 } //close if statement
 
-                //display results on page.
 
                        
 
                 var newEvent = $("<div>");
                 newEvent.addClass("card float-sm-left float-md-left float-lg-left");
-                newEvent.css("width", "22rem");
+                newEvent.css("width", "30rem");
                 newEvent.css("height", "30rem");
                 newEvent.css("margin-right", "10px");
                 newEvent.css("margin-bottom", "10px");
@@ -284,11 +292,12 @@ function getEvents(){
                 newEvent.append(neweventTitle);
                 newEvent.append(newEventImage);
                 newEvent.append(newEventcardbody);
-                        
 
                 //append card to html
 
+                
                 $("#eventDisplay").append(newEvent);
+               
 
                 //clear float
 
@@ -301,20 +310,16 @@ function getEvents(){
         error: function(xhr, status, err) {
             // This time, we do not end up here!
          }
+
         }); //close AJAX call
+
+
 
     }// close function
 
+//Autocomplete
 
-}); //End of Document ready
-
-
-
-
-
-
-/*
- /*   //firebase config
+ //firebase config
     var config = {
         apiKey: "AIzaSyAm2DX2UmnZ1-IG1fWL4lFJWPvx2eXV5PU",
         authDomain: "rps-game-ce388.firebaseapp.com",
@@ -338,167 +343,13 @@ function getEvents(){
         option.textContent=snapshot.val().name
         document.getElementById("eventCountry").append(option)
     
-       }) */
- 
-  
-      
-  /*  // searchURL = "//https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp&city=" + eventCity + "&" + countryCode ""=&stateCode="
+       }) 
 
-       searchURL = "//https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp&city=" + eventCity
-
-       console.log(searchURL);
-
-$("#eventSearch").on("click", function(){
-
-  event.preventDefault();
-   
-   console.log("you clicked me!");
-    
-   //capture search values
-
-    eventCountry = $("#country").val().trim().toLowerCase();
-    console.log(eventCountry);
-
-    var code = document.getElementById("country").value;
-    console.log("Code is"+ code);
-
-    eventState = $("#state").val().trim().toLowerCase();
-    console.log(eventState);
-
-    eventCity = $("#city").val().trim().toLowerCase();
-    console.log(eventCity);
-
-    
-
-    //Ticketmaster API
- $.ajax({
-        type:"GET",
-
-        url:searchURL,
-        //url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp",
-        //url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=GRovhZWESxeRpkyVqNiWvG5iDGeyFBTp",
-        async:true,
-        dataType: "json",
-        success: function(json) {
-                    console.log(json._embedded.events);
-                    // Parse the response.
-                    // Do other things.
-        // store the data from the AJAX request in the results variable
-                    eventResults = json._embedded.events;
-
-                    for(var i=0; i<eventResults.length; i++){
-
-                        if(Array.isArray(eventResults[i].priceRanges)){
-                          
-                        console.log("Event number is: " +i);
-                        console.log("Event name is :" + eventResults[i].name);//1
-                        console.log("Event image :" + eventResults[i].images[3].url);//2
-                        console.log("Event Currency  :" + eventResults[i].priceRanges[0].currency);//3
-                        console.log("Event Min Price :" + eventResults[i].priceRanges[0].min);//4
-                        console.log("Event Max Price :" + eventResults[i].priceRanges[0].max);//5
-                        console.log("Event Local Date :" + eventResults[i].dates.start.localDate);//6
-                        console.log("Event Local Time :" + eventResults[i].dates.start.localTime);//7
-                        console.log("Event Venue - City :" + eventResults[i]._embedded.venues[0].city.name);//8
-                        console.log("Event Venue - Country:" + eventResults[i]._embedded.venues[0].country.name);//9
-                        console.log("Buy Tickets " + eventResults[i].url);
-
-                        //assign event variables based on data
-                        eventName = eventResults[i].name;
-                        eventImage = eventResults[i].images[0].url;
-                        eventCurrency = eventResults[i].priceRanges[0].currency;
-                        EventMinPrice = eventResults[i].priceRanges[0].min;
-                        eventMaxPrice = eventResults[i].priceRanges[0].max;
-                        eventDate = eventResults[i].dates.start.localDate;
-                        eventTime = eventResults[i].dates.start.localTime ;
-                        eventVenue = eventResults[i]._embedded.venues[0].city.name + " , " + eventResults[i]._embedded.venues[0].country.name;
-                        eventURL = eventResults[i].url;
-                        }
-
-                        //display results on page.
-
-                        var newEvent = $("<div>");
-                        newEvent.addClass("card float-sm-left float-md-left float-lg-left");
-                        newEvent.css("width", "18rem");
-
-                        var newEventImage = $("<img>");
-                        newEventImage.attr("src", eventResults[i].images[0].url);
-                        newEventImage.addClass("card-img-top");
-
-                        var newEventcardbody = $("<div>");
-                        newEventcardbody.addClass("card-body");
-                        newEventcardbody.attr("data-min", EventMinPrice);
-                        newEventcardbody.attr("data-max", eventMaxPrice);
-
-                        
-                        var neweventTitle = $("<h5>");
-                        neweventTitle.addClass("card-title");
-                        neweventTitle.text(eventResults[i].name);
-
-                        var neweventDetails = $("<p>");
-                        neweventDetails.addClass("card-text");
-                        neweventDetails.text(eventVenue);
-
-                        var newEventPrice = $("<p>");
-                        newEventPrice.addClass("card-text");
-                        newEventPrice.text("Price range: " + EventMinPrice + " to " + eventMaxPrice + " " + eventCurrency);
-
-                        var newEventDate
-                        newEventDate = $("<p>");
-                        newEventDate.addClass("card-body");
-                        newEventDate.text(eventDate + " , " + eventTime);
-    
-
-                        var buyTickets = $("<a>");
-                        buyTickets.addClass("btn btn-primary");
-                        buyTickets.attr("href", eventResults[i].url);
-                        buyTickets.text("Buy Tickets");
-
-                        //append event details to card body    
-                        
-                        newEventcardbody.append(newEventPrice);
-                        newEventcardbody.append(newEventDate);
-                        newEventcardbody.append(neweventDetails);
-                        newEventcardbody.append(buyTickets);
-
-                        //append image and card body to card
-                        newEvent.append(neweventTitle);
-                        newEvent.append(newEventImage);
-                        newEvent.append(newEventcardbody);
-                        
-
-                        //append card to html
-
-                        $("#eventDisplay").append(newEvent);
-
-                        //clear float
-
-                        var clearFloat = $("<div>");
-                        clearFloat.addClass("clearfix");
-
-                    }
-
-                    $("#eventDisplay").attr("data-currency", eventCurrency);
-
-
-
-                    
-
-                 },
-        error: function(xhr, status, err) {
-                    // This time, we do not end up here!
-                 }
-
-                  
-    
-
-      });
-
-//       $('.flexdatalist').flexdatalist({
-//         minLength: 1,
-//         valueProperty:"value"
-//    });
- 
-
+$('.flexdatalist').flexdatalist({
+    minLength: 1,
+    valueProperty:"value"
 });
 
-});*/
+}); //End of Document ready
+
+
